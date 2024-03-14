d6d741a1fb1a436917cbdb6eb996bd787aaaccdf
devDharamveer <dharamveer.singh@infobeans.com>
2024-03-14 17:31:23 +0530

    dashboard5

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Dashboard from "./modules/dashboard/Dashboard";
 import FoodDetails from "./modules/foodDetails/FoodDetails";
 import Cart from "./modules/cart/Cart";
@@ -6,12 +7,32 @@ import ReactGA from "react-ga4";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactGA.initialize("G-J7NLMZ94BL");
-ReactGA.send({
-  hitType: "pageview",
-  page: window.location.pathname,
-});
 
 function App() {
+  const [userEmail, setUserEmail] = useState<string | null>(null);
+
+  useEffect(() => {
+    // Here, you would implement your custom authentication logic
+    // For demonstration purposes, let's assume you have a function `getUserEmail` that retrieves the user's email
+    const userEmail = getUserEmail();
+    if (userEmail) {
+      ReactGA.send({
+        hitType: "event",
+        category: "user",
+        action: "login",
+        label: userEmail,
+      });
+      setUserEmail(userEmail);
+    }
+  }, []);
+
+  // Function to simulate retrieving user's email (replace with your custom logic)
+  const getUserEmail = () => {
+    // Implement your logic to retrieve the user's email here
+    // For demonstration purposes, let's assume the user's email is stored in localStorage
+    return localStorage.getItem("userEmail");
+  };
+
   return (
     <div className="App">
       <Routes>
