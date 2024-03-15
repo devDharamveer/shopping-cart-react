9eb15d23fb123750e70c06f3181a85fc3a3caf91
devDharamveer <dharamveer.singh@infobeans.com>
2024-03-15 19:04:27 +0530

    dashboard user email 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,18 @@
+import React, { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./modules/dashboard/Dashboard";
 import FoodDetails from "./modules/foodDetails/FoodDetails";
 import Cart from "./modules/cart/Cart";
-import ReactGA from "react-ga4";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-ReactGA.initialize("G-J7NLMZ94BL", { testMode: true });
-ReactGA.send({
-  hitType: "pageview",
-  page: window.location.pathname,
-});
-ReactGA._toGtagOptions({
-  testMode: true,
-});
 
 function App() {
-  // Sending the custom event when the component mounts
-  ReactGA.event({
-    category: 'button',
-    action: 'successful_sign_in-dharam@gmail.com',
-    label: 'dharamveersinghkhera@gmail.com',
-    value: 99,
-
-  });
+  useEffect(() => {
+    // Sending the custom event when the component mounts
+    gtag('event', 'successful_sign_in', {
+      'event_category': 'button',
+      'event_label': 'dharamveersinghkhera@gmail.com',
+      'value': 99
+    });
+  }, []);
 
   return (
     <div className="App">
@@ -36,3 +26,7 @@ function App() {
 }
 
 export default App;
+function gtag(arg0: string, arg1: string, arg2: { event_category: string; event_label: string; value: number; }) {
+  throw new Error("Function not implemented.");
+}
+
