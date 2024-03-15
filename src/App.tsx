28b1f1a237388b93604334f4af5d1d47f87c7412
devDharamveer <dharamveer.singh@infobeans.com>
2024-03-15 15:37:07 +0530

    dashboard user emailwith gtag

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./modules/dashboard/Dashboard";
 import FoodDetails from "./modules/foodDetails/FoodDetails";
 import Cart from "./modules/cart/Cart";
-import ReactGA from "react-ga4";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactGA.initialize("G-J7NLMZ94BL");
-ReactGA.send({
-  hitType: "pageview",
-  page: window.location.pathname,
-});
-
 function App() {
-  // Sending the custom event when the component mounts
-  ReactGA.event({
-    category: 'button',
-    action: 'successful_sign_in',
-    label: 'label',
-  }, {
-    email: 'this.authUserEmail' // Replace 'this.authUserEmail' with the actual user's email
-  });
+  useEffect(() => {
+    // Sending the custom event when the component mounts
+    sendCustomEvent();
+  }, []);
+
+  function sendCustomEvent() {
+    // Replace 'successful_sign_in', 'button', 'label', and 'this.authUserEmail@gmail.com' with your actual event details
+    (window as any).gtag('event', 'successful_sign_in', {
+      'event_category': 'button',
+      'event_label': 'label',
+      'value': 'Sign in with google',
+      'email': 'this.authUserEmail@gmail.com' // Replace 'this.authUserEmail' with the actual user's email
+    });
+  }
 
   return (
     <div className="App">
